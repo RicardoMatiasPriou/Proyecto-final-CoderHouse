ad605bcf755b1313a6b8f25d2c4a537945586671
RicardoMatiasPriou <rpriouborla@gmail.com>
2020-12-19 12:56:05 -0300

    00.00.12
    Agregar popup para compras

---
+++ js/PopUps.js
@@ -0,0 +1,3 @@
+var textPopupSaludo1 ='<div class="overlay active" id="overlay"><div class="popup active" id="popup"><a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times"></i></a><h3>Hola '+ Cliente1.nombre+'</h3>'
+var textPopupSaludo2 ='<h5>Sos menor de edad algunos productos han sido bloqueados</h5>'
+var MenorDeEdad = '</div></div>'
--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', async (e) => {
         var precioXDocena = doc.data().precioXDocena
         var img = doc.data().img
 
-        var texXunidad = '<div class="box"><div class="imgBx"><img src="' + img + '"class="fitBg1"></div><div class="content"><h2>' + comida + '<br><span>Pecio=' + precioXUnidad + '</span></h2></div></div>';
+        var texXunidad = '<div class="box" data-name="'+comida+'"data-img="'+img+'" ><div class="imgBx"><img src="' + img + '" class="fitBg1"></div><div class="content"><h2>' + comida + '<br><span>Pecio=' + precioXUnidad + '</span></h2></div></div>';
         ///////// Agregar Tarjeta de Producto /////////
         $("#comidass").append(texXunidad);
         console.log( comida,
@@ -24,6 +24,17 @@ window.addEventListener('DOMContentLoaded', async (e) => {
             precioXDocena,
             img);
     });
+    productosss =  document.querySelectorAll('div.box')
+    console.log(productosss);
+    productosss.forEach(function(item){
+        item.addEventListener('click',function(){
+            var compra = '<div class="overlay active" id="overlay"><div class="popup active" id="popup"><a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times"onclick="Remove()"></i></a><form id="reguistroDeProductos" enctype="multipart/form-data"><div class="form-group"><h1>'+ item.dataset.name +'</h1><br><img src="'+item.dataset.img+'" alt="ProductoAComorar"><div class="form-row "><div class="col-sm-2 my-2"><label for="PrecioPorDocena">Cantidad</label><div class="input-group"></div><input type="number" class="form-control" id="PrecioPorDocena"placeholder="Precio por docena" min=0 autofocus></div></div></div><button class="btn btn-primary btn-lg btn-block" id="AgregarAlCarrito" onclick="Remove()">Comprar</button></form></div></div>'
+            $("#PopUps").append(compra);
+            console.log(item.dataset.name);
+
+        })
+
+    })
 })
 /////////////////////////    Usuario    ///////////////////////////////////////////////////
 function Cliente(nombre, edad) {
@@ -31,45 +42,48 @@ function Cliente(nombre, edad) {
     this.edad = edad;
 }
 var Cliente1 = []
-usuario()
-
+window.addEventListener('DOMContentLoaded', e => {
+    usuario()
+})
 function usuario() {
-
+        /////////////// Chequeo el Local Storaje   /////////////////////
     var registro = localStorage.length
     if (registro != 0) {
         Cliente1 = JSON.parse(localStorage.getItem("name"))
-        ImprimirNombreUno()
         ConsultarEdad()
 
     } else {
 
         Cliente1 = new Cliente(prompt("Diga Su Nombre"), prompt("Diga Su Edad"));
         localStorage.setItem("name", JSON.stringify(Cliente1))
-        ImprimirNombre()
         ConsultarEdad()
 
     }
 }
 
 function ImprimirNombre() {
-    alert("Hola " + Cliente1.nombre)
+var textPopupSaludo1 ='<div class="overlay active" id="overlay"><div class="popup active" id="popup"><a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times" onclick="Remove()"></i></a><h3>Hola '+ Cliente1.nombre+'</h3>'
+var textPopupSaludo2 = '</div></div>'
+    $("#PopUps").append(textPopupSaludo1,textPopupSaludo2);
     $("#name").append("Hola " + Cliente1.nombre);
 }
-
-function ImprimirNombreUno() {
-    alert("Hola de nuevo " + Cliente1.nombre)
-    $("#name").append("Hola " + Cliente1.nombre);
+function ImprimirNombreMenor18() {
+    var textPopupSaludo1 ='<div class="overlay active" id="overlay"><div class="popup active" id="popup"><a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times" onclick="Remove()"></i></a><h3>Hola '+ Cliente1.nombre+'</h3><h5>Sos menor de edad algunos productos han sido bloqueados</h5></div></div>'
+        $("#PopUps").append(textPopupSaludo1);
+        $("#name").append("Hola " + Cliente1.nombre);
+    }
+function Remove(){
+	$("#PopUps").empty();
 }
 //////////////////////////////// Bloquear Bebidas Alcholicas/////////////////////////////////////////////////////////////////
 function ConsultarEdad() {
     if (Cliente1.edad > 17) {
-        alert("Sos mayor de edad productos desbloquedados")
+        ImprimirNombre()
     } else {
         $("#Bebidasss").empty();
         $("#Bebidas").empty();
         $(".Bebidass").remove();
-
-        alert("Sos menor de edad productos bloquedados")
+        ImprimirNombreMenor18()
     }
 }
 
@@ -133,3 +147,6 @@ function limpiarStoraje() {
     localStorage.clear();
     location.reload();
 }
+
+var productosss 
+
